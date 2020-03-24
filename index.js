f3240a490c8b8ce95eb9dbde2d537c039d5c97f5
Kevin Boston <kbos22@gmail.com>
2020-03-24 15:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+
+function destructivelyAppendKitten(arr, name){
+  arr = arr.pop();
+  arr.push(name);
+  return arr;
+}
+function destructivelyPrependKitten(arr, name){
+  arr.unshift();
+  arr.shift(name);
+  return arr;
+}
+function destructivelyRemoveLastKitten(arr){
+  arr = arr.pop();
+  return arr;
+}
+function destructivelyRemoveFirstKitten(arr){
+  arr.shift();
+  return arr;
+}
+function appendKitten(arr, name){
+  var newArr = arr.concat(name);
+  return newArr;
+}
+function removeFirstKitten(arr){
+  return arr.slice();
+}
